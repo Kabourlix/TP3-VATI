124ec5e9647dc49338b4a1b204563ed30d983763
HaritianaRakoto <122493070+HaritianaRakoto@users.noreply.github.com>
2023-03-30 17:13:50 -0400

    Update performances.py
    modification du code pour tester le model du VGG
    test lancé sur 5 images de chaque classe dans le test dataset
    affichage de la prédiction pour chaque image

--- performances.py
+++ performances.py
@@ -1,56 +1,80 @@
+import os
+
 import matplotlib.pyplot as plt
 import numpy as np
-import tensorflow as tf
-import seaborn as sns
-from tensorflow import keras
+
+from keras.optimizers.optimizer_v1 import adam
+
 from sklearn.metrics import make_scorer, confusion_matrix, cohen_kappa_score, classification_report
-from sklearn.model_selection import cross_val_score
 
-#history = model.fit(X_train,y_train,verbose=1,epochs=50, batch_size=64)
+from pre_traitement import test_generator
+from keras.models import load_model
+import keras.utils as image
 
-def show_performance(model,history,X_test,y_test):
-    # Evaluation du modele
-    score = model.evaluate(X_test, y_test, verbose=0)
-    print(f"Test loss: {score[0]:4.4f}")
-    print(f"Test accuracy: {score[1]:4.4f}")
+# Load image test
 
-    # prediction
-    y_pred = np.argmax(model.predict(X_test), axis=-1)
+img_path = []
+datatest_path = "D:/UQAC hiver 2023/8INF804-Vision artificielle/TP3-VATI/test"
 
-    # Trace les courbes d'apprentissage et de validation
-    loss = history.history['loss']
-    val_loss = history.history['val_loss']
-    epochs = range(1, len(loss) + 1)
-    plt.plot(epochs, loss, 'y', label='Training loss')
-    plt.plot(epochs, val_loss, 'r', label='Validation loss')
-    plt.title('Training and validation loss')
-    plt.xlabel('Epochs')
-    plt.ylabel('Loss')
-    plt.legend()
+# Load 5 images in each folder of the test dataset
+for folder_name in os.listdir(datatest_path):
+    folder_path = os.path.join(datatest_path, folder_name)
+    if os.path.isdir(folder_path):
+        images_to_test = 5
+        images_tested = 0
+        for file in os.listdir(folder_path):
+            if images_tested < images_to_test:
+                img_path.append(os.path.join(folder_path, file))
+                images_tested += 1
+            else:
+                break
+
+X_test = []
+for img in img_path:
+    img_test = image.load_img(img, target_size=(150, 150))
+    img_arr = image.img_to_array(img_test) / 255.0
+    X_test.append(img_arr)
+
+X_test = np.array(X_test)
+y_true = []
+for i in range(len(X_test)):
+    folder_name = os.path.basename(os.path.dirname(img_path[i]))
+    y_true.append(folder_name)
+
+# Define the custom_objects dictionary with the custom optimizer
+custom_objects = {'custom_optimizer': adam}
+
+# Load the model with the custom_objects argument
+saved_model = load_model('vggclf.h5', custom_objects=custom_objects)
+
+label_mapping = dict([(v, k) for k, v in test_generator.class_indices.items()])  # on associe chaque id à un label
+
+# Make predictions
+predictions = saved_model.predict(X_test)
+y_pred = np.argmax(predictions, axis=-1)
+y_pred_label = [label_mapping[int(prediction_id)] for prediction_id in y_pred]
+
+# Compute the Cohen's kappa coefficient
+kappa = cohen_kappa_score(y_true, y_pred_label)
+# Print the kappa score
+print("Cohen's kappa coefficient: ", kappa)
+
+# Print the classification report and confusion matrix
+print("Results of the test set:")
+print(classification_report(y_true, y_pred_label))
+
+# Show each image with the prediction
+for i, img in enumerate(img_path):
+    img_test = image.load_img(img, target_size=(150, 150))
+    img_arr = image.img_to_array(img_test) / 255.0
+    img_input = img_arr.reshape((1, img_arr.shape[0], img_arr.shape[1], img_arr.shape[2]))
+
+    prediction_id = np.argmax(saved_model.predict(img_input), axis=-1)
+    prediction_label = label_mapping[int(prediction_id)]
+
+    fig, ax = plt.subplots(figsize=(5, 5))
+    ax.imshow(img_test)
+    ax.set_title(prediction_label)
+    ax.axis('off')
     plt.show()
 
-    # accuracy graph
-    acc = history.history['accuracy']
-    val_acc = history.history['val_accuracy']
-    plt.plot(epochs, acc, 'y', label='Training accuracy')
-    plt.plot(epochs, val_acc, 'r', label='Validation accuracy')
-    plt.title('Training and validation accuracy')
-    plt.xlabel('Epochs')
-    plt.ylabel('Accuracy')
-    plt.legend()
-    plt.show()
-
-    # Compute the Cohen's kappa coefficient
-    y_true = y_test[:, -1]
-    kappa = cohen_kappa_score(y_true, y_pred)
-    # Print the kappa score
-    print("Cohen's kappa coefficient: ", kappa)
-
-    # affiche le rapport de classification
-    print("Results of the test set:")
-    print(classification_report(y_test, y_pred))
-
-    # affiche la matrice de confusion
-    cm = confusion_matrix(y_test, y_pred)
-    sns.heatmap(cm, annot=True)
-    plt.show()
